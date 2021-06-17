1c2e02dc134ee80fae1a3a776165e4490fbe57bc
yousef fawzy <yousef.fawzy.2000@gmail.com>
2021-06-17 19:57:58 +0300

    Add project files.

---
+++ ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleResturantSystem
+{
+    public class ViewModel
+    {
+        public IList<Dish> Dishes { get; set; }
+
+    }
+}
